3db52b3bb4b4be193a02c9692503897c6a4e69d6
mec0825 <m.macrocosm@gmail.com>
2016-02-24 16:59:20 +0800

    add mathJax

--- header.php
+++ header.php
@@ -11,6 +11,9 @@
   ga('create', 'UA-32627494-1', 'auto');
   ga('send', 'pageview');
     </script>
+    <script type="text/javascript" async
+      src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
+    </script>
     <?php wp_head(); ?>
 </head>
 <body>
